d335248d9819a6b09b60f60703dfa3288ee9e7d3
Jakub Mamelski <jakub.mamelski@outlook.com>
2021-01-28 20:33:02 +0100

    Add empty Tests project

---
+++ Recuitment.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recuitment.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
