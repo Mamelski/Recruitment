0e6c2a7550aaee33648a22b1362708ed8b2f710b
Jakub Mamelski <jakub.mamelski@outlook.com>
2021-01-29 22:58:43 +0100

    Add HashController

---
+++ Recruitment.API/Controllers/HashController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Recruitment.API.Services;
+using Recruitment.Contracts;
+
+namespace Recruitment.API.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class HashController : ControllerBase
+    {
+        private readonly ILogger<HashController> _logger;
+        private readonly IHashService _hashService;
+
+        public HashController(ILogger<HashController> logger, IHashService hashService)
+        {
+            _logger = logger;
+            _hashService = hashService;
+        }
+
+        [HttpPost("hash")]
+        public async Task<IActionResult> GetHash([FromBody] Credentials credentials)
+        {
+            _logger.LogInformation("Hash endpoint called with user login {Login}", credentials.Login);
+            
+            var hash = await _hashService.GetHash(credentials);
+            
+            return Ok(hash);
+        }
+    }
+}
