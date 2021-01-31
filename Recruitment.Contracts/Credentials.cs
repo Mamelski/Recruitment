a24d102320e9d1150fbeb0f3460b9d6fee9bca64
Jakub Mamelski <jakub.mamelski@outlook.com>
2021-01-31 13:17:34 +0100

    Add HashController_Should tests

--- Recruitment.Contracts/Credentials.cs
+++ Recruitment.Contracts/Credentials.cs
@@ -1,8 +1,12 @@
-﻿namespace Recruitment.Contracts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recruitment.Contracts
 {
     public class Credentials
     {
+        [Required]
         public string Login { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
---
+++ Recruitment.Tests/HashController_Should.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Recruitment.API.Controllers;
+using Recruitment.API.Services;
+using Recruitment.Contracts;
+using Shouldly;
+using Xunit;
+
+namespace Recruitment.Tests
+{
+    public class HashController_Should
+    {
+        [Theory, AutoData]
+        public async Task Call_Hash_service_with_correct_credentials(
+            Mock<ILogger<HashController>> logger,
+            Mock<IHashService> hashServiceMock,
+            Credentials credentials)
+        {
+            var sut = new HashController(logger.Object, hashServiceMock.Object);
+
+            await sut.GetHash(credentials);
+            
+            hashServiceMock.Verify(hashService => hashService.GetHash(credentials), Times.Once);
+        }
+
+        [Theory, AutoData]
+        public async Task Return_hash_for_correct_credentials(
+            Mock<ILogger<HashController>> logger,
+            Mock<IHashService> hashServiceMock,
+            Credentials credentials,
+            string hash)
+        {
+            hashServiceMock.Setup(hashService => hashService.GetHash(credentials))
+                .ReturnsAsync(hash);
+            
+            var sut = new HashController(logger.Object, hashServiceMock.Object);
+
+            var response = await sut.GetHash(credentials) as OkObjectResult;
+            
+            response.ShouldNotBeNull();
+        }
+    }
+}
